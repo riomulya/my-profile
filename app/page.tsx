de294f9d98f33912e71dca72856c70cba0eeda74
RioMulya <riomulya75@gmail.com>
2024-03-19 16:13:28 +0700

    delete background beams

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { Sertification } from "@/components/Pages/sertification";
 import Hero from "@/components/Layout/hero";
-import { BackgroundBeams } from "@/components/ui/background-beams";
 import Projects from "@/components/Pages/projects";
 import { Education } from "@/components/Pages/education";
 
@@ -10,8 +9,6 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <div className="h-auto relative w-full overflow-hidden  flex flex-col items-center justify-center rounded-lg">
-        {/* <Boxes /> */}
-        <BackgroundBeams />
         <Hero />
         <Sertification />
         <Projects />
